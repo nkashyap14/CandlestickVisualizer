70169aaff852dde95365742af0307470cd7ff51c
nkashyap14 <nikhilesh.kashyap2903@gmail.com>
2023-05-07 19:59:30 -0400

    adding comments

--- candlestickvisualizer/data_processor.py
+++ candlestickvisualizer/data_processor.py
@@ -14,14 +14,17 @@ class DataProcessor:
         pass
 
     def transform_data(self, stocks, data):
+        '''Transforming the data into proper structure as for downstream visualization purposes'''
         tickerData = defaultdict(list)
         for ticker in stocks:
             for date in data.index:
                 tempEntry = {"date" : date.date().strftime("%Y-%m-%d")}
                 for option in self.__options__:
                     tempEntry[option] = data[(option, ticker)].loc[date]
+                    #if any of the options are missing a value remove the date key so as to ensure we don't collect this entry
                     if pd.isna(data[(option, ticker)].loc[date]) and "date" in tempEntry.keys():
                         del tempEntry["date"]
+                #If there is a missing value for this point in time than we drop the date
                 if len(tempEntry.keys()) == 6:
                     tickerData[ticker].append(tempEntry)
             tickerData[ticker] = pd.DataFrame(tickerData[ticker])
